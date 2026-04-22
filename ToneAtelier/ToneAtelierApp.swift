b1d9d7d6d9153983d3b7ca214a21b65989597249
LCH-1228 <chanho1228@gmail.com>
2026-04-22 17:36:10 +0900

    프로젝트 파일 추가

---
+++ ToneAtelier/ToneAtelierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneAtelierApp.swift
+//  ToneAtelier
+//
+//  Created by LCH on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ToneAtelierApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ ToneAtelierTests/ToneAtelierTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToneAtelierTests.swift
+//  ToneAtelierTests
+//
+//  Created by LCH on 4/22/26.
+//
+
+import Testing
+@testable import ToneAtelier
+
+struct ToneAtelierTests {
+
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
+}
