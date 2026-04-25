27457b26e4f151372b7903c430501e67c5006213
LCH-1228 <chanho1228@gmail.com>
2026-04-25 21:49:43 +0900

    refactor: 정적 자산 접근 격리 정리 - Swift concurrency 경고를 정리를 위해 HomeCategory와 AppAsset의 정적 접근자를 nonisolated로 수정

--- ToneAtelier/Shared/DesignSystem/AppAsset.swift
+++ ToneAtelier/Shared/DesignSystem/AppAsset.swift
@@ -7,28 +7,28 @@
 
 enum AppAsset {
   enum HomeCategory {
-    static let food = "icon-category-food"
-    static let people = "icon-category-people"
-    static let landscape = "icon-category-landscape"
-    static let night = "icon-category-night"
-    static let star = "icon-category-star"
+    nonisolated static let food = "icon-category-food"
+    nonisolated static let people = "icon-category-people"
+    nonisolated static let landscape = "icon-category-landscape"
+    nonisolated static let night = "icon-category-night"
+    nonisolated static let star = "icon-category-star"
   }
 
   enum Common {
-    static let heartFilled = "icon-heart-filled"
-    static let heartOutline = "icon-heart-outline"
+    nonisolated static let heartFilled = "icon-heart-filled"
+    nonisolated static let heartOutline = "icon-heart-outline"
   }
 
   enum MainTab {
-    static let homeFilled = "tab-home-filled"
-    static let homeOutline = "tab-home-outline"
-    static let feedFilled = "tab-feed-filled"
-    static let feedOutline = "tab-feed-outline"
-    static let filterFilled = "tab-filter-filled"
-    static let filterOutline = "tab-filter-outline"
-    static let searchFilled = "tab-search-filled"
-    static let searchOutline = "tab-search-outline"
-    static let profileFilled = "tab-profile-filled"
-    static let profileOutline = "tab-profile-outline"
+    nonisolated static let homeFilled = "tab-home-filled"
+    nonisolated static let homeOutline = "tab-home-outline"
+    nonisolated static let feedFilled = "tab-feed-filled"
+    nonisolated static let feedOutline = "tab-feed-outline"
+    nonisolated static let filterFilled = "tab-filter-filled"
+    nonisolated static let filterOutline = "tab-filter-outline"
+    nonisolated static let searchFilled = "tab-search-filled"
+    nonisolated static let searchOutline = "tab-search-outline"
+    nonisolated static let profileFilled = "tab-profile-filled"
+    nonisolated static let profileOutline = "tab-profile-outline"
   }
 }
